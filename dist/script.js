077911300d04764ddb16b51958b9245a01a8ff1b
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2023-10-29 04:57:11 +0000

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,11 @@
+// animate_on_scroll
+const typed = new Typed('#element', {
+    strings: ['Front-End Developer', 'Technical Writer'],
+    typeSpeed: 90,
+    backSpeed:20,
+    loop:true,
+    
+    
+  
+  });
+  
